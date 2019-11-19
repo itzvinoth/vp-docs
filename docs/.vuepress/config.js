5e808384ad829813fab3e1ff81277c6ae996ff08
Vinoth Kumar V <vinoth.kumar@zerodha.com>
2019-11-19 20:39:19 +0530

    new folders added

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,34 +11,71 @@ module.exports = {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
+      { text: 'Base', link: '/base/' },
       { text: 'Github', link: 'https://zerodha.com' },
     ],
-    sidebar: {
-      '/guide/': [
-        {
-          title: 'Guide',
-          collapsable: false,
-          children: [
-            '',
-            'installation',
-            'color',
-            'button',
-            'input',
-            'form',
-            'select',
-            'radio',
-            'checkbox',
-            'switch',
-            'modal',
-            'toast',
-            'table',
-            'table-sorting',
-            'table-limit',
-            'table-csv',
-            'table-toolbar'
-          ],
-        },
-      ],
-    },
+    sidebar: [
+      {
+        title: 'Guide',
+        collapsable: false,
+        children: [
+          '/',
+          '/guide/installation'
+        ]
+      },
+      {
+        title: 'Base',
+        collapsable: false,
+        sidebarDepth: 1,
+        children: [
+          {
+            title: 'General',
+            collapsable: false,
+            children: [
+              '/general/button',
+              '/general/color',
+            ]
+          },
+          {
+            title: 'Data entry',
+            collapsable: false,
+            children: [
+              '/data-entry/input',
+              '/data-entry/form',
+              '/data-entry/select',
+              '/data-entry/radio',
+              '/data-entry/checkbox',
+              '/data-entry/switch',
+            ]
+          },
+        ]
+      },
+      {
+        title: 'View',
+        collapsable: false,
+        sidebarDepth: 1,
+        children: [
+          {
+            title: 'Display',
+            collapsable: false,
+            children: [
+              '/display/modal',
+              '/display/toast',
+            ]
+          },
+          {
+            title: 'Table',
+            collapsable: false,
+            children: [
+              '/table/table',
+              '/table/table-csv',
+              '/table/table-limit',
+              '/table/table-sorting',
+              '/table/table-toolbar',
+            ]
+          },
+        ]
+      }
+    ]
   }
 }
